3bcc74f2c43c453abab5f87699f93d459748f058
이강인 <108244613+Kangin-Lee@users.noreply.github.com>
2022-12-27 17:25:39 +0900

    css만 하면 됨

--- JS/todo.js
+++ JS/todo.js
@@ -1,21 +1,26 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = document.querySelector("#todo-form input");
-const toDos = [];
+let toDos = [];
+
+const TODOS_KEY = "todos";
 
 const saveToDos =()=>{
-    localStorage.setItem("todos",JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
 }
 
 const deleteToDo=(event)=>{
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id != parseInt(li.id));
+    saveToDos();
 }
 
 const paintToDo=(newTodo)=>{
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText=newTodo;
+    span.innerText=newTodo.text;
     const button = document.createElement("button");
     button.innerText="❌";
     button.addEventListener("click", deleteToDo);
@@ -29,9 +34,21 @@ const handleToDoSubmit = (event)=>{
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj={
+        text: newTodo,
+        id: Data.now()
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos != null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
